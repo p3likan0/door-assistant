113ff3ddf285be58d6000671aa26bfadc9ec9262
Esteban A. Bosse H <estebanbosse@gmail.com>
2017-10-01 22:16:55 -0300

    Add test enable door

--- integration_tests.py
+++ integration_tests.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import unittest
+import json
 import shlex
 import subprocess
 from time import sleep
@@ -11,6 +12,8 @@ PASSWORD = "pobaxx"
 GEN_PASS_RPC = "/bin/bash generate_user_and_password_for_rpc.sh {} {}"
 RPC_CRED = 'mos --rpc-creds "{}:{}"'
 GET_CONF = "call Config.Get"
+SET_CONF = "config-set"
+ENABLE_DOOR = "door.enable=true"
 
 
 class TestStringMethods(unittest.TestCase):
@@ -39,6 +42,24 @@ class TestStringMethods(unittest.TestCase):
         p = subprocess.Popen(args)
         exit_code = p.wait()
         self.assertEqual(exit_code, 0)
+
+    def test_enable_door(self):
+        args = shlex.split((RPC_CRED).format(USER, PASSWORD))
+        args += shlex.split(SET_CONF)
+        args += shlex.split(ENABLE_DOOR)
+        p = subprocess.Popen(args)
+        exit_code = p.wait()
+        self.assertEqual(exit_code, 0)
+        sleep(3)
+
+        args = shlex.split((RPC_CRED).format(USER, PASSWORD))
+        args += shlex.split(GET_CONF)
+        args.append(json.dumps({"key": "door.enable"}))
+        p = subprocess.Popen(args, stdout=subprocess.PIPE)
+        exit_code = p.wait()
+        self.assertEqual(exit_code, 0)
+        self.assertEqual(p.stdout.read().decode('utf-8').rstrip(), "true")
+        p.stdout.close()
 
 
 if __name__ == '__main__':
